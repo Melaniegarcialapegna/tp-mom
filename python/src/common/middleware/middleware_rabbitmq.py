7d8bd2917357340072c47e4f21783b7ddb133ca8
Melaniegarcialapegna <mgarcial@fi.uba.ar>
2026-09-10 22:11:42 -0300

    feat: add the instanciation of the MessageMiddlewareQueueRabbitMQ

--- python/src/common/middleware/middleware_rabbitmq.py
+++ python/src/common/middleware/middleware_rabbitmq.py
@@ -6,7 +6,15 @@ from .middleware import MessageMiddlewareQueue, MessageMiddlewareExchange
 class MessageMiddlewareQueueRabbitMQ(MessageMiddlewareQueue):
 
     def __init__(self, host, queue_name):
-        pass
+        self.queue_name = queue_name
+
+        # Establish a blocking connection to RabbitMQ server
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=host))
+
+        self.channel = self.connection.channel()
+
+        # Declare a queue with the specified name
+        self.channel.queue_declare(queue=self.queue_name)
 
 class MessageMiddlewareExchangeRabbitMQ(MessageMiddlewareExchange):
     
